f4ad35169b60dbbea81ac2e64b3f67ba310c0f56
Ansh Prajapati <ansh07github@gmail.com>
2025-07-30 15:11:25 +0530

    Add files via upload

---
+++ U1_P2.py
@@ -0,0 +1,9 @@
+#Write a program to display sum of two complex numbers.
+
+
+a=complex(input("Enter a 1st complex number:(e.g..3+1j)="))
+b=complex(input("Enter a 2nd complex number:(e.g..2+5j)="))
+
+sum=a+b
+
+print("Sum of Two complex number is ",sum)

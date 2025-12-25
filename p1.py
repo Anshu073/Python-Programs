299c3510453a893f051a631cd06060a65637bbee
Ansh Prajapati <ansh07github@gmail.com>
2025-12-25 22:30:02 +0530

    Add files via upload
    U3

---
+++ p1.py
@@ -0,0 +1,12 @@
+class student:
+    def __init__(self,n,name,clg):
+        self.n=n
+        self.name=name
+        self.clg=clg
+    def display(self):
+        print("name:",self.name)
+        print("no:",s1.n)
+        print("clg:",self.clg)
+
+s1=student(1,"ansh","pica")
+s1.display()
